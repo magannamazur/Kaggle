112c382155af12ffb61556484ef12434f0d3c73f
Magdalena Mazur <mag.anna.mazur@gmail.com>
2023-04-18 19:58:36 +0200

    DataFrame

--- Pandas/creating_data.py
+++ Pandas/creating_data.py
@@ -3,3 +3,6 @@ import pandas as pd
 # DataFrame
 DF = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
 print(DF)
+
+DF_string = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
+print(DF_string)
